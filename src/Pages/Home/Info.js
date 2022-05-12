27b922c1ad5521cdda64383ae545a1e82677047a
rahathossain3 <rahathossain210@gmail.com>
2022-05-12 21:11:32 +0600

    card works

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -2,14 +2,15 @@ import React from 'react';
 import InfoCard from './InfoCard';
 import clock from '../../assets/icons/clock.svg';
 import marker from '../../assets/icons/marker.svg';
-import quote from '../../assets/icons/quote.svg';
+import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
+
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
+            <InfoCard cardTitle="Opening Hours" img={clock}></InfoCard>
+            <InfoCard cardTitle="Our Location" img={marker}></InfoCard>
+            <InfoCard cardTitle="Contact Us" img={phone}></InfoCard>
         </div>
     );
 };
