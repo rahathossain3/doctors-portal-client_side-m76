4c61b72b4f79198ce04fee31119ad1e337e46d6d
rahathossain3 <rahathossain210@gmail.com>
2022-05-13 23:39:20 +0600

    appointment page add

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -13,9 +13,9 @@ const MakeAppointment = () => {
                 <img className='mt-[-100px]' src={doctor} alt="" />
             </div>
 
-            <div className='flex-1 p-4'>
+            <div className='flex-1 px-5'>
                 <h3 className='text-xl text-primary font-bold pb-3'>Appointment</h3>
-                <h2 className='text-3xl text-white pb-3'>Make An Appointment Today</h2>
+                <h2 className='text-3xl text-white py-5'>Make An Appointment Today</h2>
                 <p className=' text-white pb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat perferendis voluptate repellendus hic consequuntur dicta eligendi, atque non dolor nihil pariatur mollitia facilis assumenda corrupti in eum impedit explicabo. Corporis dolorem reiciendis quia voluptatem numquam aliquid rem beatae assumenda eum!</p>
                 <PrimaryButton>Get Started</PrimaryButton>
             </div>
