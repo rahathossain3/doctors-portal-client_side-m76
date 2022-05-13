4418e1f53a7f936ca739b60d794363b6d6e8b5f7
rahathossain3 <rahathossain210@gmail.com>
2022-05-13 18:51:41 +0600

    footer add, contact from add, all class make className

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
+import ContactFrom from './ContactFrom';
+import Footer from './Footer';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
@@ -13,6 +15,8 @@ const Home = () => {
             <Services></Services>
             <MakeAppointment></MakeAppointment>
             <Testimonials></Testimonials>
+            <ContactFrom></ContactFrom>
+            <Footer></Footer>
         </div>
     );
 };
