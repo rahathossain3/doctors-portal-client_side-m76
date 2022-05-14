67f8d3611f8a1ddc1827f1919c8e672c395bbbcb
rahathossain3 <rahathossain210@gmail.com>
2022-05-15 03:16:23 +0600

    connect mongodb data loaded for sercices

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -13,7 +13,7 @@ const AvailableAppointment = ({ date }) => {
 
 
     useEffect(() => {
-        fetch('services.json')
+        fetch('http://localhost:5000/service')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
