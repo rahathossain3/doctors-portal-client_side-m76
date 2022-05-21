fe7524e150b4824c51f2db261d03bd600f477ab5
rahathossain3 <rahathossain210@gmail.com>
2022-05-21 17:23:24 +0600

    dashboard add with sidebar, nave icon, table add

--- src/Pages/Dashboard.js/MyAppointments.js
+++ src/Pages/Dashboard.js/MyAppointments.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyAppointments = () => {
+
+    const [appointment, setAppointment] = useState([]);
+
+    const [user, loading, error] = useAuthState(auth);
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/booking?patient=${user.email}`)
+                .then(res => res.json())
+                .then(data => setAppointment(data));
+        }
+    }, [user])
+
     return (
         <div>
-            <h2>My appointment</h2>
+            <h2>My appointment : {appointment.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Treatment</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <!-- row 1 --> */}
+
+                        {
+                            appointment.map(a => <tr>
+                                <th>1</th>
+                                <td>{a.patientName}</td>
+                                <td>{a.date}</td>
+                                <td>{a.slot}</td>
+                                <td>{a.treatment}</td>
+                            </tr>
+                            )
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
