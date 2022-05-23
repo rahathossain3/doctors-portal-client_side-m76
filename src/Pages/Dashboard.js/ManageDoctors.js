16f294d12ebf2703be533979aaf5df6d6b5bed3b
rahathossain3 <rahathossain210@gmail.com>
2022-05-23 18:39:43 +0600

    doctor delete with jwt

--- src/Pages/Dashboard.js/ManageDoctors.js
+++ src/Pages/Dashboard.js/ManageDoctors.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
 
     //react query 
-    const { data: doctors, isLoading } = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
+    const { data: doctors, isLoading, refetch } = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
@@ -18,6 +19,32 @@ const ManageDoctors = () => {
     return (
         <div>
             <h2 className="text-2xl">Manage Doctor: {doctors.length}</h2>
+
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, index) => <DoctorRow
+                                key={doctor._key}
+                                doctor={doctor}
+                                index={index}
+                                refetch={refetch}
+                            > </DoctorRow>)
+                        }
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
